53001bb102655c312a8d5f25fe52aae4993e1bdc
Prashant7970 <prashant887359@gmail.com>
2022-10-02 04:15:16 +0530

    adding new changes

--- demo.js
+++ demo.js
@@ -1,19 +1,26 @@
-let movieData= document.querySelector("#form")
-let movieArr=JSON.parse(localStorage.getItem("movie-list"))||[];
-let count=0;
-movieData.addEventListener("submit",function(el){
+let publicData= document.querySelector("#form")
+let publicArr=JSON.parse(localStorage.getItem("users"))||[];
+
+publicData.addEventListener("submit",function(el){
 el.preventDefault();
-if(movieData.name.value==""){
+if(publicData.name.value==""||publicData.email.value==""||publicData.phone.value==""||publicData.size.value=="company size"){
 alert("please fill all the details")
 } else{
-    count++;
-    localStorage.setItem("count",count)
+   
     let obj={
-        movie_name:movieData.name.value,
+        name:publicData.name.value,
+        email:publicData.email.value,
+        mob:publicData.phone.value,
+        company:publicData.company.value,
+        Csize:publicData.size.value,
+        indusName:publicData.industry.value,
+        agents:publicData.Agent.value,
+        area:publicData.PROCESSING_INSTRUCTION_NODE.value
+
       
     }
-    movieArr.push(obj)
-    localStorage.setItem("movie-list",JSON.stringify(movieArr))
+    publicArr.push(obj)
+    localStorage.setItem("users",JSON.stringify(publicArr))
   
 }
    
